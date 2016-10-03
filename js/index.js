7bf9d855fd5215aa9cfc02a53d189892cb1a4927
rodionovp@gmail.com <XJNQjNcCpr23wmAam>
2016-10-02 19:44:51 -0700

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 exports.handler = function (event, context, callback) {
-    console.log(event)
-    console.log(context)
-    console.log(callback)
-}
+    console.log(event);
+    console.log(context);
+    console.log(callback);
+};
